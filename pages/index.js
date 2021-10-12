8bf7fb037c994b9637e3ba06de4ac8d55b4bab62
Khriztian Moreno <khriztianmoreno@gmail.com>
2021-10-11 21:47:58 -0500

    Basic background-image change (#2)

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,16 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 import Header from '../components/Header';
+import SideImage from '../components/SideImage';
 
 export default function Home() {
   return (
     <>
       <Header />
       <main className={styles.main}>
-        <div className={styles.imageContainer}>image</div>
+        <div className={styles.imageContainer}>
+          <SideImage />
+        </div>
         <div className={styles.formContainer}>form</div>
       </main>
     </>
