d01ac71333ca019c89e9c1729f18ebc2d69bc30c
Khriztian Moreno <khriztianmoreno@gmail.com>
2021-10-11 20:37:40 -0500

    Adding layout base

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+function Header() {
+  return (
+    <Head>
+      <title>Tienda Experiencias</title>
+      <meta name="description" content="Tienda Experiencias" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
+
+export default Header;
