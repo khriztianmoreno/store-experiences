1da5b785afaa06f1330eedd8218bacea79c0e676
Khriztian Moreno <khriztianmoreno@gmail.com>
2021-11-23 20:39:38 -0500

    Update the layout of the success pagee (#14)

--- pages/success.js
+++ pages/success.js
@@ -1,27 +1,63 @@
-import { Main, Container } from '../styles/Success';
+import { useRouter } from 'next/router';
+
 import Header from '../components/Header';
 import CopyRight from '../components/CopyRight';
 import Logo, { LogoMobile } from '../components/Logo';
 
+import {
+  Circle,
+  Container,
+  InstagramLink,
+  Main,
+  MessageContaienr,
+  Messages,
+  Root,
+  Title,
+} from '../styles/Success';
+
 function Success() {
+  const { query } = useRouter();
+
+  const profile = query?.type;
+
   return (
-    <>
+    <Root>
       <Header />
+      <Logo small />
+      <LogoMobile />
       <Main>
-        <Container>
-          <h2>¡Registro exitoso!</h2>
-          <p>
-            Ahora haces parte de la comunidad de planes, viajes y experiencias.
-            El lugar donde encontrarás lo ideal para regalar y vivir tus tiempos
-            libres. Espera próximamente noticias en tu correo.
-          </p>
-          <h3>Las mejores cosas de la vida no son cosas. Son momentos</h3>
-          <Logo />
-          <LogoMobile />
-        </Container>
-        <CopyRight />
+        <div>
+          <Container>
+            <Title>¡Registro exitoso!</Title>
+            <MessageContaienr>
+              <Circle />
+              <Messages>
+                <p>
+                  Ahora haces parte de la comunidad de planes, viajes y
+                  experiencias. El lugar donde encontrarás lo ideal para regalar
+                  y vivir tus tiempos libres.
+                </p>
+                {profile === 'supplier' ? (
+                  <p>Espera próximamente noticias en tu correo.</p>
+                ) : (
+                  <>
+                    <p>Te estaremos contactando próximamente.</p>
+                    <p>
+                      Síguenos en Instagram{' '}
+                      <InstagramLink href="https://instagram.com">
+                        @TiendaExperiencias
+                      </InstagramLink>
+                    </p>
+                  </>
+                )}
+              </Messages>
+            </MessageContaienr>
+            <h3>Las mejores cosas de la vida no son cosas. Son momentos</h3>
+          </Container>
+          <CopyRight />
+        </div>
       </Main>
-    </>
+    </Root>
   );
 }
 
--- styles/Success.js
+++ styles/Success.js
@@ -1,20 +1,99 @@
 import styled, { css } from 'styled-components';
 
+export const Root = styled('div')(
+  (props) => css`
+    background-color: ${props.theme.colors.white};
+    min-height: 100vh;
+  `
+);
+
+const heightNavbar = '78px';
+
 export const Main = styled('main')(
   (props) => css`
-    display: flex;
-    flex-direction: column-reverse;
-    position: relative;
     color: ${props.theme.colors.stratos};
-    background-color: ${props.theme.colors.white};
-    margin-top: 50px;
-    padding: 40px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    @media (min-width: 992px) {
+      align-content: space-between;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      min-height: 100vh;
+    }
+
+    & > div {
+      width: 100%;
+
+      @media (min-width: 992px) {
+        width: 400px;
+      }
+    }
   `
 );
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  padding-left: 30px;
+  padding-right: 30px;
+  padding-top: 64px;
+
+  h3 {
+    text-align: center;
+  }
+
+  @media (min-width: 992px) {
+    padding: 0;
+  }
+`;
+
+export const Title = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 15px;
+  margin-bottom: 65px;
+  text-align: center;
+
+  @media (min-width: 992px) {
+    margin-top: 100px;
+  }
+`;
+
+export const MessageContaienr = styled.div`
+  position: relative;
+  min-height: 220px;
+`;
+
+export const InstagramLink = styled('a')(
+  (props) => css`
+    color: ${props.theme.colors.electricViolet};
+  `
+);
+
+export const Messages = styled.div`
+  position: absolute;
+  z-index: 2;
+
+  @media (min-width: 992px) {
+    left: 0;
+    margin-left: auto;
+    margin-right: auto;
+    position: absolute;
+    right: 0;
+    width: 400px;
+  }
+`;
+
+export const Circle = styled.div`
+  background-color: #efa893;
+  border-radius: 50%;
+  height: 208px;
+  left: 0;
+  margin-left: auto;
+  margin-right: auto;
+  position: absolute;
+  right: 0;
+  top: -30px;
+  width: 208px;
 `;
