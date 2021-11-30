41914c21029eccd247171f3e73ad92ef0bae6b07
Khriztian Moreno <khriztianmoreno@gmail.com>
2021-11-30 12:00:16 -0500

    Update SEO data (#19)

--- components/Seo/index.jsx
+++ components/Seo/index.jsx
@@ -1,7 +1,7 @@
 const Seo = (props) => {
   const {
     title = 'Planes para el día a día - Tienda Experiencias',
-    description = 'Descripción: Regala y regálate experiencias, planes y momentos que te dejen grandes recuerdos. Con Tienda Experiencias los tienes todos en un solo lugar. Conócelos ahora.',
+    description = 'Regala y regálate experiencias, planes y momentos que te dejen grandes recuerdos. Con Tienda Experiencias los tienes todos en un solo lugar. Conócelos ahora.',
     url = 'https://www.tiendaexperiencias.com/',
     image = 'https://res.cloudinary.com/khriztianmoreno/image/upload/v1638290590/tienda_experiencias/Imagen-preview.jpg',
   } = props;
